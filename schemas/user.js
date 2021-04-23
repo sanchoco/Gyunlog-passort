a9b54c00610a8d2cd533dad490041f1cf476dd0a
sanchoco <jguuun@gmail.com>
2021-04-24 02:58:52 +0900

    로그인 페이지 버튼 위치 수정, passport 변수명 변경

--- schemas/user.js
+++ schemas/user.js
@@ -5,7 +5,7 @@ const user = new Schema({
 	id: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	nickname: { type: String, required: true, unique: true },
-	social: { type: String }
+	provider: { type: String }
 });
 
 module.exports = mongoose.model('User', user);
