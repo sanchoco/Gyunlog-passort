34a6274d35b2421da0560d74c903c38f54e89c3f
sanchoco <jguuun@gmail.com>
2021-06-04 00:40:23 +0900

    회원가입 검증 부분 주석 추가

--- source/verification.js
+++ source/verification.js
@@ -7,6 +7,7 @@ function check_id(id) {
 	return true;
 }
 
+// 비밀번호가 4자 이상이고 아이디가 포함되선 안됨.
 function check_password(id, password) {
 	if (password.length < 4) return false;
 	if (password.includes(id)) return false;
@@ -14,6 +15,7 @@ function check_password(id, password) {
 	return true;
 }
 
+// 닉네임이 2자 이상이고 10자 미만, 공백이 없는 경우만 경우만 통과
 function check_nickname(nickname) {
 	if (nickname.length < 2) return false;
 	if (nickname.length > 10) return false;
